d81b5b82978dc1ecbefc98b2ff12f6c048e00623
Daniel Carrizo <hdcarrizo30@gmail.com>
2023-12-09 11:24:29 -0300

    correcciones primera entrega

--- main.js
+++ main.js
@@ -1,33 +1,59 @@
 class ProductManager {
     constructor() {
-        this.products =[];
+        this.products = [];
     }
 
-    static id= 0;
+    static id = 0;
 
-    addProduct(title,description,price,image,code,stock) {
+    validateProperty(property, propertyName) {
+        if (!property) {
+            console.log(`Invalid ${propertyName}. ${propertyName} must be provided.`);
+            return false;
+        }
+        return true;
+    }
+
+    addProduct(title, description, price, image, code, stock) {
+        // Validation
+        if (
+            !this.validateProperty(title, 'title') ||
+            !this.validateProperty(description, 'description') ||
+            !this.validateProperty(price, 'price') ||
+            !this.validateProperty(image, 'image') ||
+            !this.validateProperty(code, 'code') ||
+            !this.validateProperty(stock, 'stock')
+        ) {
+            return;
+        }
+
+        if (this.products.some(product => product.code === code)) {
+            console.log("Product with the same code already exists.");
+            return;
+        }
+
         ProductManager.id++;
-        this.products.push ({title,description,price,image,code,stock,id:ProductManager.id});
+        this.products.push({ title, description, price, image, code, stock, id: ProductManager.id });
     }
 
-    getProduct(){
+    getProduct() {
         return this.products;
     }
 
     getProductById(id) {
-        if (this.products.find((producto)=> producto.id === id)){
-            console.log ("Not found");
-        } else{
-        console.log(this.products.find((producto)=> producto.id === id));
+        const product = this.products.find(producto => producto.id === id);
+        if (!product) {
+            console.log("Product not found");
+            return;
         }
+        return product;
     }
 }
 
+const productos = new ProductManager;
 
-const productos= new ProductManager;
-
-productos.addProduct("titulo1","descripcion1", 1000,"imagen1", "abc123",5);
-productos.addProduct("titulo2","descripcion2", 1000,"imagen2", "abc123",5);
+productos.addProduct("titulo1", "descripcion1", 1000, "imagen1", "abc123", 5);
+productos.addProduct("titulo2", "descripcion2", 1000, "imagen2", "abc123", 5); // Code repetition, should print an error
+productos.addProduct("titulo3", "descripcion3", 1200, "imagen3", "xyz456", 8);
 console.log(productos.getProduct());
 
-productManager.getProductById(1); 
+productos.getProductById(1);
